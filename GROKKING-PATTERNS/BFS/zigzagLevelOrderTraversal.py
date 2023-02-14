d488351d374ff1232bdef6983e027a9cac100be4
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-02-14 01:25:47 -0600

    Added solution to zigzag level order traversal

---
+++ GROKKING-PATTERNS/BFS/zigzagLevelOrderTraversal.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+def zigzagLevelOrderTraversal(root):
+
+    if root == None:
+        return []
+
+    queue = deque()
+    queue.append(root)
+    level = 1
+    levelCount = 0
+    totalCount = len(queue)
+    levelRes = []
+    totalRes = []
+
+
+    while queue:
+        currentNode = queue.popleft()
+        leveCount += 1
+        levelRes.append(currentNode.val)
+
+        if currentNode.left:
+            queue.append(currentNode.left)
+        if currentNode.right:
+            queue.append(currentNode.right)
+        
+        if levelCount == totalCount:
+            
+            if level % 2 != 0:
+                totalRes.append(levelRes)
+            else:
+                totalRes.append(levelRes[::-1])
+            
+            level += 1
+            levelRes = []
+            levelCount = 0 
+            totalCount = len(queue)
+
+    return totalRes
