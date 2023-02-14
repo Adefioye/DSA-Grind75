136a911dcba8bb8679f41006c818ecf883fdfc3a
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-02-14 01:44:35 -0600

    Added solution to average of levels in a binary tree

---
+++ GROKKING-PATTERNS/BFS/averageOfLevelsInBinaryTree.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+
+def averageOfLevels(root):
+
+    if root == None:
+        return []
+
+    queue = deque()
+    queue.append(root)
+    totalRes = []
+    levelRes = []
+    levelCount = 0
+    totalCount = len(queue)
+
+    while queue:
+        currentNode = queue.popleft()
+        levelCount += 1
+        levelRes.append(currentNode.val)
+
+        if currentNode.left:
+            queue.append(currentNode.left)
+        if currentNode.right:
+            queue.append(currentNode.right)
+
+        if levelCount == totalCount:
+            average = sum(levelRes)/levelCount
+            totalRes.append(average)
+
+            levelRes = []
+            levelCount = 0 
+            totalCount = len(queue)
+        
+    return totalRes
