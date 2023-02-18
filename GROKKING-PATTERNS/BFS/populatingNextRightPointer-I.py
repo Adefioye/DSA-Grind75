777059bc353833997cbbad24f5f11f02dfed609b
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-02-18 14:39:46 -0600

    Added optimal solution to populating next right pointers I

--- GROKKING-PATTERNS/BFS/populatingNextRightPointer-I.py
+++ GROKKING-PATTERNS/BFS/populatingNextRightPointer-I.py
@@ -1,32 +1,54 @@
 from collections import deque
 
+
+## OPTIMAL SOLUTION
+
 def populatingNextRightPointers(root):
+    curr, nxt = root, root.left if root else None
 
-    if root == None:
-        return []
+    while curr and nxt:
 
-    queue = deque()
-    queue.append(root)
+        curr.left.next = curr.right 
 
-    while queue:
+        if curr.next:
+            curr.right.next = curr.next.left
 
-        levelRes = queue()
+        curr = curr.next
 
-        for _ in range(len(queue)):
-            currentNode = queue.popleft()
-            levelRes.append(currentNode)
-
-            if currentNode.left:
-                queue.append(currentNode.left)
-            if currentNode.right:
-                queue.append(currentNode.right)
-        
-        
-        prev = levelRes.popleft()
-        while levelRes:
-            currentNode = levelRes.popleft()
-            prev.next = currentNode 
-            prev = currentNode
-
+        if not curr:
+            curr = nxt
+            nxt = curr.left
 
     return root
+
+# def populatingNextRightPointers(root):
+
+#     if root == None:
+#         return []
+
+#     queue = deque()
+#     queue.append(root)
+
+#     while queue:
+
+#         levelRes = queue()
+
+#         for _ in range(len(queue)):
+#             currentNode = queue.popleft()
+#             levelRes.append(currentNode)
+
+#             if currentNode.left:
+#                 queue.append(currentNode.left)
+#             if currentNode.right:
+#                 queue.append(currentNode.right)
+        
+
+#         prev = levelRes.popleft()
+#         while levelRes:
+#             currentNode = levelRes.popleft()
+#             prev.next = currentNode 
+#             prev = currentNode
+
+
+#     return root
+
