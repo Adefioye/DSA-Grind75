5d4f627d9422d96ef2cf0b722178ddd906f5bc64
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-02-21 12:10:36 -0600

    Added a neat constant space time solution for conflicting appointments

--- GROKKING-PATTERNS/MERGE-INTERVALS/conflicting-appointments.py
+++ GROKKING-PATTERNS/MERGE-INTERVALS/conflicting-appointments.py
@@ -6,23 +6,57 @@ appointments3 = [[4,5], [2,3], [3,6]]
 #false
 
 def conflictingAppointments(appointments):
+
+    appointments.sort(key=lambda x: x[0])
+
     l = 0
-    r = l + 1
 
-    appointments.sort(key=lambda item: item[0])
-
-    while r < len(appointments):
+    for r in range(1, len(appointments)):
         s1, e1 = appointments[l]
         s2, e2 = appointments[r]
 
-        if e1 >= s2  and e2 >= s1:
+        if s2 < e1:
             return False
-        
+
         l += 1
-        r = l + 1
-    
+
     return True
 
 print(conflictingAppointments(appointments1))
 print(conflictingAppointments(appointments2))
 print(conflictingAppointments(appointments3))
+
+# def conflictingAppointments(appointments):
+
+#     appointments.sort(key=lambda x: x[0])
+
+#     output = [appointments[0]]
+
+#     for start, end in appointments[1:]:
+#         prevEnd = output[-1][1]
+
+#         if start < prevEnd:
+#             return False
+        
+#         output.append([start, end])
+    
+#     return True
+
+
+# def conflictingAppointments(appointments):
+#     l = 0
+#     r = l + 1
+
+#     appointments.sort(key=lambda item: item[0])
+
+#     while r < len(appointments):
+#         s1, e1 = appointments[l]
+#         s2, e2 = appointments[r]
+
+#         if e1 >= s2  and e2 >= s1:
+#             return False
+        
+#         l += 1
+#         r = l + 1
+    
+#     return True
