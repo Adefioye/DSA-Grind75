cea46bf14761b9eb1b95f7cda95101267490fc38
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-02-24 01:37:53 -0600

    Added optimal solution to sum root to leaf nums

--- GROKKING-PATTERNS/DFS/sumRootToLeafNums.py
+++ GROKKING-PATTERNS/DFS/sumRootToLeafNums.py
@@ -1,17 +1,40 @@
+#OPTIMAL SOLUTION
 
 def sumNumbers(root):
 
-    return dfs(root, "", 0)
+    return dfs(root, 0)
 
-def dfs(root, res, total):
+def dfs(root, pathSum):
 
-    if root == None:
-        return 0 
+    if root is None:
+        return 0
+    
+    pathSum = (pathSum * 10) + root.val
 
     if root.left is None and root.right is None:
-        return total + int(res + str(root.val))
-    
-    leftRes = dfs(root.left, res + str(root.val), total)
-    rightRes = dfs(root.right, res + str(root.val), total)
+        return pathSum
+
+    leftRes = dfs(root.left, pathSum)
+    rightRes = dfs(root.right, pathSum)
 
     return leftRes + rightRes
+
+
+#   NON OPTIMAL solution
+
+# def sumNumbers(root):
+
+#     return dfs(root, "", 0)
+
+# def dfs(root, res, total):
+
+#     if root == None:
+#         return 0 
+
+#     if root.left is None and root.right is None:
+#         return total + int(res + str(root.val))
+    
+#     leftRes = dfs(root.left, res + str(root.val), total)
+#     rightRes = dfs(root.right, res + str(root.val), total)
+
+#     return leftRes + rightRes
