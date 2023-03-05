a1b7f26dab51c5a1f39b100d3056e9006c0beac7
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-03-05 12:10:46 -0600

    Added solution to number of islands problem

---
+++ GROKKING-PATTERNS/ISLANDS/number_of_islands.py
@@ -0,0 +1,58 @@
+grid1 = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+#Output: 1
+
+grid2 = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+#Output: 3
+
+from collections import deque
+
+def numIslands(grid):
+
+    if len(grid) == 0 or len(grid[0]) == 0:
+        return 0 
+    
+    count = 0
+    NROWS = len(grid)
+    NCOLS = len(grid[0])
+    LAND = "1"
+    WATER = "0"
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+    for r in range(NROWS):
+        for c in range(NROWS):
+
+            if grid[r][c] == LAND:
+                count += 1 
+
+                queue = deque()
+                queue.append((r, c))
+                grid[r][c] = WATER 
+
+
+                while queue:
+                    curPos = queue.popleft()
+
+                    for direction in directions:
+                        row = curPos[0] + direction[0]
+                        col = curPos[1] + direction[1]
+
+                        if row < 0 or row >= NROWS or col < 0 or col >= NCOLS or grid[row][col] == WATER:
+                            continue 
+
+                        queue.append((row, col))
+                        grid[row][col] = WATER 
+
+    return count
+
+print(numIslands(grid1))
+print(numIslands(grid2))
