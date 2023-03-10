da9c7281db3fed8d64ccd1221b4a96713920d069
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-03-10 17:43:46 -0600

    Added iterative solution for letter case permutation problem

--- GROKKING-PATTERNS/BACKTRACKING/letterCasePermutation.py
+++ GROKKING-PATTERNS/BACKTRACKING/letterCasePermutation.py
@@ -3,28 +3,48 @@ s1 = "a1b2"
 s2 = "3z4"
 #Output: ["3z4","3Z4"]
 
-def letterCasePermutation(chars: str):
+def letterCasePermutation(chars):
 
-    if len(chars) == 0:
-        return [""]
-    
-    firstChar = chars[0]
-    rest = chars[1:]
+    outputs = [""]
 
-    permsWithoutFirst = letterCasePermutation(rest)
-    allPermutations = []
+    for char in chars:
+        temp = []
 
-    if firstChar.isalpha():
-        # Its a character 
-        for item in permsWithoutFirst:
-            allPermutations.append(firstChar.lower() + item)
-            allPermutations.append(firstChar.upper() + item)
-    else:
-        for item in permsWithoutFirst:
-            allPermutations.append(firstChar + item)
+        if char.isalpha():
 
+            for output in outputs:
+                temp.append(output + char.lower())
+                temp.append(output + char.upper())
+        else:
+            for output in outputs:
+                temp.append(output + char)
 
-    return allPermutations 
+        outputs = temp 
+
+    return outputs
 
 print(letterCasePermutation(s1))
 print(letterCasePermutation(s2))
+
+# def letterCasePermutation(chars: str):
+
+#     if len(chars) == 0:
+#         return [""]
+    
+#     firstChar = chars[0]
+#     rest = chars[1:]
+
+#     permsWithoutFirst = letterCasePermutation(rest)
+#     allPermutations = []
+
+#     if firstChar.isalpha():
+#         # Its a character 
+#         for item in permsWithoutFirst:
+#             allPermutations.append(firstChar.lower() + item)
+#             allPermutations.append(firstChar.upper() + item)
+#     else:
+#         for item in permsWithoutFirst:
+#             allPermutations.append(firstChar + item)
+
+
+#     return allPermutations 
