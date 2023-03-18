88673ee57cf06a854716b67655abaeaf5d64d307
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-03-18 10:24:56 -0500

    Added iterative solution for subsets problem

--- GROKKING-PATTERNS/BACKTRACKING/subsets.py
+++ GROKKING-PATTERNS/BACKTRACKING/subsets.py
@@ -5,19 +5,33 @@ nums2 = [0]
 
 def subsets(nums):
 
-    if len(nums) == 0:
-        return [ [] ]
-    
-    first = nums[0]
-    rest = nums[1:]
-    combsWithoutFirst = subsets(rest)
-    combsWithFirst = []
+    powerSet = [[]]
 
-    for item in combsWithoutFirst:
-        combWithFirst = [*item, first]
-        combsWithFirst.append(combWithFirst)
+    for num in nums:
+        temps = [*powerSet]
 
-    return [*combsWithFirst, *combsWithoutFirst]
+        for temp in temps:
+            powerSet.append(temp + [num])
+
+    return powerSet
 
 print(subsets(nums1))
 print(subsets(nums2))
+
+
+## RECURSIVE SOLUTION FOR SUBSETS
+# def subsets(nums):
+
+#     if len(nums) == 0:
+#         return [ [] ]
+    
+#     first = nums[0]
+#     rest = nums[1:]
+#     combsWithoutFirst = subsets(rest)
+#     combsWithFirst = []
+
+#     for item in combsWithoutFirst:
+#         combWithFirst = [*item, first]
+#         combsWithFirst.append(combWithFirst)
+
+#     return [*combsWithFirst, *combsWithoutFirst]
