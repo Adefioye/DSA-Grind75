5d5e9e1e0066f3aae14f4d127867415c60f6d289
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-03-19 15:18:59 -0500

    Added backtracking solution for permutation problem

--- GROKKING-PATTERNS/BACKTRACKING/permutation.py
+++ GROKKING-PATTERNS/BACKTRACKING/permutation.py
@@ -5,24 +5,43 @@ nums2 = [0,1]
 nums3 = [1]
 # Output: [[1]]
 
+# BACKTRACKING APPROACH
+
 def permutation(nums):
+    res = []
+    def backtrack(nums, path):
 
-    if len(nums) == 0:
-        return [[]]
-    
-    first = nums[0]
-    rest = nums[1:]
-    permsWithoutFirst = permutation(rest)
-    allPermutations = []
+        if len(nums) == 0:
+            res.append(path)
+            return 
+        
+        for i in range(len(nums)):
+            backtrack(nums[:i] + nums[i+1:], path + [nums[i]])
 
-    for i, item in enumerate(permsWithoutFirst):
+    backtrack(nums, [])
 
-        for i in range(len(item) + 1):
-            permWithFirst = [*item[:i], first, *item[i:]]
-            allPermutations.append(permWithFirst)
+    return res
 
-    return allPermutations
 
 print(permutation(nums1))
 print(permutation(nums2))
 print(permutation(nums3))
+
+# NON-BACKTRACKING APPROACH
+# def permutation(nums):
+
+#     if len(nums) == 0:
+#         return [[]]
+    
+#     first = nums[0]
+#     rest = nums[1:]
+#     permsWithoutFirst = permutation(rest)
+#     allPermutations = []
+
+#     for i, item in enumerate(permsWithoutFirst):
+
+#         for i in range(len(item) + 1):
+#             permWithFirst = [*item[:i], first, *item[i:]]
+#             allPermutations.append(permWithFirst)
+
+#     return allPermutations
