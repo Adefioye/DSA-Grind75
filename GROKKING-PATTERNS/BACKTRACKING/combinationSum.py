ca0f4600b1301abaefb93b7aa5d93cb4a04dcd09
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-03-22 00:33:03 -0500

    Added second recursive solution to combination sum problem

--- GROKKING-PATTERNS/BACKTRACKING/combinationSum.py
+++ GROKKING-PATTERNS/BACKTRACKING/combinationSum.py
@@ -6,30 +6,56 @@ candidates3 = [2]; target3 = 1
 # Output: []
 
 def combinationSum(candidates, target):
-    
+
     res = []
     comb = []
 
-    def backtrack(start):
-        
-        if sum(comb) > target:
-            return 
-    
-        if sum(comb) == target:
+    def backtrack(i, comb, total):
+
+        if total == target:
             res.append(comb[:])
             return 
-    
-        for i in range(start, len(candidates)):
-            comb.append(candidates[i])
+        
+        if i >= len(candidates) or total > target:
+            return 
+        
+        comb.append(candidates[i])
+        backtrack(i, comb, total + candidates[i])
+        
+        comb.pop()
+        backtrack(i + 1, comb, total)
 
-            backtrack(i)
-
-            comb.pop()
-
-    backtrack(0)
+    backtrack(0, [], 0)
 
     return res
+
 
 print(combinationSum(candidates1, target1))
 print(combinationSum(candidates2, target2))
 print(combinationSum(candidates3, target3))
+
+## FIRST BACKTRACKING SOLUTION
+# def combinationSum(candidates, target):
+    
+#     res = []
+#     comb = []
+
+#     def backtrack(start):
+        
+#         if sum(comb) > target:
+#             return 
+    
+#         if sum(comb) == target:
+#             res.append(comb[:])
+#             return 
+    
+#         for i in range(start, len(candidates)):
+#             comb.append(candidates[i])
+
+#             backtrack(i)
+
+#             comb.pop()
+
+#     backtrack(0)
+
+#     return res
