a0e0d774c8977c115f7b0236d68f1891b11d51d4
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-03-25 21:13:14 -0500

    Added second solution to reverse sublist using dummynode

--- GROKKING-PATTERNS/LINKED-LIST-REVERSAL/reverse-sublist.py
+++ GROKKING-PATTERNS/LINKED-LIST-REVERSAL/reverse-sublist.py
@@ -1,33 +1,66 @@
+from typing import ListNode 
 
 def reverseBetween(head, left, right):
 
+    dummyNode = ListNode(0, head)
+    prevLeftNode = head 
+    pos = 0 
     current = head
-    pos = 1
-    startNode = head
 
-    while pos < left:
-        startNode = current
+    while current and pos < left:
+        prevLeftNode = current
         current = current.next
         pos += 1
 
-    tailNode = current
-    prevNode = None
+    tailNode = current 
+    prev = None
 
-    while pos >= left and pos <= right:
-        nextNode = current.next
-        current.next = prevNode
-        prevNode = current
-        current = nextNode
-        pos += 1
+    while current and pos >= left and pos <= right:
+        nxt = current.next
+        current.next = prev 
+        prev = current 
+        current = nxt 
 
-    startNode.next = prevNode
-    tailNode.next = current
+    prevLeftNode.next = prev 
+    tailNode.next = current 
 
     if left > 1:
-        return head
+        return head 
+    
+    return prev
+
+
+
+    return
+# def reverseBetween(head, left, right):
+
+#     current = head
+#     pos = 1
+#     startNode = head
+
+#     while pos < left:
+#         startNode = current
+#         current = current.next
+#         pos += 1
+
+#     tailNode = current
+#     prevNode = None
+
+#     while pos >= left and pos <= right:
+#         nextNode = current.next
+#         current.next = prevNode
+#         prevNode = current
+#         current = nextNode
+#         pos += 1
+
+#     startNode.next = prevNode
+#     tailNode.next = current
+
+#     if left > 1:
+#         return head
 
         
-    return prevNode
+#     return prevNode
 
 
 
