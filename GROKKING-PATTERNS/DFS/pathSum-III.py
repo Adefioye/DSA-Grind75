25a18f568cb1af290595dc1bb9694c8cf6b9d688
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-03-30 18:19:27 -0500

    Added linear solution to path sum III problem

--- GROKKING-PATTERNS/DFS/pathSum-III.py
+++ GROKKING-PATTERNS/DFS/pathSum-III.py
@@ -1,35 +1,65 @@
 
-
-## SECOND SOLUTION: time = O (nlogn)
+## THIRD SOLUTION: time = O (n)
 class Solution:
     def pathSum(self, root, targetSum):
 
-        self.total = 0 
-        self.path = []
+        self.count = 0 
+        freq = {0 : 1} 
 
-        def dfs(root):
+        def dfs(root, prevSum):
 
             if root is None:
                 return 
             
-            self.path.append(root.val)
+            curSum = root.val + prevSum 
+            x = curSum - targetSum 
 
-            dfs(root.left)
-            dfs(root.right)
+            if x in freq:
+                self.count += freq[x]
 
-            tempSum = 0
-            for i in range(len(self.path) - 1, -1, -1):
-                tempSum += self.path[i]
+            if curSum in freq:
+                freq[curSum] += 1 
+            else:
+                freq[curSum] = 1 
 
-                if tempSum == targetSum:
-                    self.total += 1 
+            dfs(root.left, curSum)
+            dfs(root.right, curSum)
+            freq[curSum] -= 1 
 
-            # For removing last item added the path
-            self.path.pop()
+        dfs(root, 0) 
 
-        dfs(root)
+        return self.count
 
-        return self.total 
+## SECOND SOLUTION: time = O (nlogn)
+# class Solution:
+#     def pathSum(self, root, targetSum):
+
+#         self.total = 0 
+#         self.path = []
+
+#         def dfs(root):
+
+#             if root is None:
+#                 return 
+            
+#             self.path.append(root.val)
+
+#             dfs(root.left)
+#             dfs(root.right)
+
+#             tempSum = 0
+#             for i in range(len(self.path) - 1, -1, -1):
+#                 tempSum += self.path[i]
+
+#                 if tempSum == targetSum:
+#                     self.total += 1 
+
+#             # For removing last item added the path
+#             self.path.pop()
+
+#         dfs(root)
+
+#         return self.total 
     
 
 ## FIRST SOLUTION: time = O(n ^ 2)
