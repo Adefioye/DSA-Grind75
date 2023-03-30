588afa532013553c0e4bb1132f1684f238cd0b22
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-03-30 01:09:09 -0500

    Added second solution to find all path sum problem

--- GROKKING-PATTERNS/DFS/findAllPathSum.py
+++ GROKKING-PATTERNS/DFS/findAllPathSum.py
@@ -1,22 +1,48 @@
 
+## SECOND SOLUTION 
+
 def pathSum(root, targetSum):
 
-    if root is None:
-        return []
+    res = [] 
 
-    return findPaths(root, targetSum, [], [])
+    def findPaths(root, pathSum, path):
 
-def findPaths(node, sums, res, total):
+        if root is None:
+            return 
+        
+        if root.left is None and root.right is None:
 
-    if node.left is None and node.right is None:
-        if sums == node.val:
-            res.append(node.val)
-            total.append(total)
+            if (root.val + pathSum) == targetSum:
+                res.append(path + [root.val]) 
+                return 
+            
+        findPaths(root.left, pathSum + root.val, path + [root.val])
+        findPaths(root.right, pathSum + root.val, path + [root.val])
 
-    if node.left:
-        findPaths(node.left, sums - node.val, res + [node.val], total)
-    if node.right:
-        findPaths(node.right, sums - node.val, res + [node.val], total)
+    findPaths(root, 0, [])
 
-    return total
+    return res
+
+## FIRST SOLUTION 
+
+# def pathSum(root, targetSum):
+
+#     if root is None:
+#         return []
+
+#     return findPaths(root, targetSum, [], [])
+
+# def findPaths(node, sums, res, total):
+
+#     if node.left is None and node.right is None:
+#         if sums == node.val:
+#             res.append(node.val)
+#             total.append(total)
+
+#     if node.left:
+#         findPaths(node.left, sums - node.val, res + [node.val], total)
+#     if node.right:
+#         findPaths(node.right, sums - node.val, res + [node.val], total)
+
+#     return total
 
