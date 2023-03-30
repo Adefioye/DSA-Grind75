201790f9802fbdeecdb958dd8caf7bacb05811fa
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-03-29 22:58:05 -0500

    Added cleaner solution for root to leaf path sum problem

--- GROKKING-PATTERNS/DFS/rootToLeafPathSum.py
+++ GROKKING-PATTERNS/DFS/rootToLeafPathSum.py
@@ -1,17 +1,40 @@
-def rootToLeafPathSum(root, targetSum, total):
 
-    if root == None:
-        return False
 
-    total += root.val
+def hasPathSum(root, targetSum):
 
-    if root.left == None and root.right == None:
-        return total == targetSum
+    def dfs(root, targetSum, pathSum):
 
-    leftRes = rootToLeafPathSum(root.left, targetSum, total)
-    rightRes = rootToLeafPathSum(root.right, targetSum, total)
+        if root is None:
+            return False 
+        
+        if root.left is None and root.right is None:
 
-    return leftRes or rightRes 
+            if (root.val + pathSum) == targetSum:
+                return True 
+            else:
+                return False 
+            
+        leftRes = dfs(root.left, targetSum, pathSum + root.val)
+        rightRes = dfs(root.right, targetSum, pathSum + root.val)
+
+        return leftRes or rightRes 
+    
+    return dfs(root, targetSum, 0)
+
+# def rootToLeafPathSum(root, targetSum, total):
+
+#     if root == None:
+#         return False
+
+#     total += root.val
+
+#     if root.left == None and root.right == None:
+#         return total == targetSum
+
+#     leftRes = rootToLeafPathSum(root.left, targetSum, total)
+#     rightRes = rootToLeafPathSum(root.right, targetSum, total)
+
+#     return leftRes or rightRes 
 
 
     
