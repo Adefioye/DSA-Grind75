ff4227d49d59b49a6323abd8c6fa0c64fdf04edf
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-03-29 21:31:02 -0500

    Added second solution to transpose matrix

--- GROKKING-PATTERNS/2D-ARRAYS/transpose.py
+++ GROKKING-PATTERNS/2D-ARRAYS/transpose.py
@@ -1,21 +1,37 @@
-from typing import List 
+# from typing import List 
 
 matrix_1 = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [[1,4,7],[2,5,8],[3,6,9]]
 matrix_2 = [[1,2,3],[4,5,6]]
+# Output: [[1,4],[2,5],[3,6]]
 
-def transpose(matrix: List[List[int]]) -> List[List[int]]:
-    ROWS, COLS = len(matrix), len(matrix[0])
-    res = [[0] * ROWS for _ in range(COLS)] 
+# ANOTHER TRANSPOSE METHOD
+def transpose(matrix):
+    NROWS = len(matrix)
+    NCOLS = len(matrix[0])
 
-    col = 0
-    for nums in matrix:
+    res = [[0] * NROWS for _ in range(NCOLS)]
 
-        for i, num in enumerate(nums):
-            res[i][col] = num
+    for r in range(NROWS):
+        for c in range(NCOLS):
 
-        col += 1
+            res[c][r] = matrix[r][c]
 
     return res
+
+# def transpose(matrix):
+#     ROWS, COLS = len(matrix), len(matrix[0])
+#     res = [[0] * ROWS for _ in range(COLS)] 
+
+#     col = 0
+#     for nums in matrix:
+
+#         for i, num in enumerate(nums):
+#             res[i][col] = num
+
+#         col += 1
+
+#     return res
 
 print(transpose(matrix_1))
 print(transpose(matrix_2))
