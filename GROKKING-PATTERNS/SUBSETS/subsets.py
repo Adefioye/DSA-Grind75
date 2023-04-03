ab3d68fab7a43fa88d0fff0b9b7065ba9840ca7b
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-04-03 07:24:27 -0500

    Added recursive solution to subsets problem

---
+++ GROKKING-PATTERNS/SUBSETS/subsets.py
@@ -0,0 +1,23 @@
+def subsets(nums):
+
+    res = []
+    subset = []
+
+    def dfs(i):
+
+        if i >= len(nums):
+            res.append(subset[:])
+            return 
+        
+        # Decision to include nums[i]
+        subset.append(nums[i])
+        dfs(i+1)
+
+        # Decision not to include nums[i]
+        subset.pop()
+        dfs(i+1)
+
+    dfs(0)
+
+    return res 
+
