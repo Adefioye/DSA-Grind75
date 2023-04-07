299ea00402e8948d1ec5a59332f58f1b3e4ead03
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-04-07 09:29:47 -0500

    Add a second optimal solution to first and last position of a sorted array problem

--- GROKKING-PATTERNS/MODIFIED-BINARY-SEARCH/findFirstAndLastPos.py
+++ GROKKING-PATTERNS/MODIFIED-BINARY-SEARCH/findFirstAndLastPos.py
@@ -5,6 +5,47 @@ nums2 = [1, 3, 8, 10, 15]; key2 = 10
 nums3 = [1, 3, 8, 10, 15]; key3 = 12
 # Output: [-1, -1]
 
+def searchRange(nums, target):
+
+    res = [-1, -1]
+    if not nums:
+        return res 
+    
+    if target < nums[0] or target > nums[-1]:
+        return res 
+    
+    res[0] = binarySearch(nums, target, False)
+    res[1] = binarySearch(nums, target, True)
+
+    return res 
+
+def binarySearch(nums, target, findMax):
+
+    l, r = 0, len(nums) - 1 
+    keyIndex = -1
+    while l <= r:
+
+        mid = (l + r) // 2 
+
+        if target < nums[mid]:
+            r = mid - 1 
+        elif target > nums[mid]:
+            l = mid + 1 
+        else:
+
+            keyIndex = mid
+
+            if findMax:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+    return keyIndex 
+
+print(searchRange(nums1, key1))
+print(searchRange(nums2, key2))
+print(searchRange(nums3, key3))
+
 ## FIRST SOLUTION WITHOUT FLAG
 # def numberRange(nums, key):
 
