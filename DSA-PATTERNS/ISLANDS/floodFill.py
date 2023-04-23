668e9e1a88e077ac0e66fc2e91aeae38490b07aa
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-04-23 02:33:21 -0500

    Added dfs solution to flood fill problem

--- DSA-PATTERNS/ISLANDS/floodFill.py
+++ DSA-PATTERNS/ISLANDS/floodFill.py
@@ -7,39 +7,68 @@ image2 = [[0,0,0],[0,0,0]]; sr2 = 0; sc2 = 0; color2 = 0
 
 def floodFill(image, sr, sc, color):
 
-    NROWS = len(image)
-    NCOLS = len(image[0])
-
     if image[sr][sc] == color:
-        return image
+        return image 
+    
+    target = image[sr][sc]
+    NROWS, NCOLS = len(image), len(image[0])
 
-    queue = deque()
-    # visited = set()
+    def dfs(r, c):
 
-    queue.append((sr, sc))
-    # visited.add((sr, sc))
+        rowInBound = 0 <= r < NROWS
+        colInBound = 0 <= c < NCOLS 
 
-    startingColor = image[sr][sc]
-    image[sr][sc] = color 
+        if not rowInBound or not colInBound or image[r][c] != target:
+            return 
+        
+        image[r][c] = color 
 
-    while queue:
-        curPos = queue.popleft()
+        dfs(r - 1, c)
+        dfs(r + 1, c)
+        dfs(r, c - 1)
+        dfs(r, c + 1)
 
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
-        for direction in directions:
-            row = curPos[0] + direction[0]
-            col = curPos[1] + direction[1]
-
-            if row < 0 or row >= NROWS or col < 0 or col >= NCOLS or image[row][col] != startingColor:
-                continue
-                
-            image[row][col] = color 
-            queue.append((row, col))
-            # visited.add((row, col))
+    dfs(sr, sc)
 
     return image
-    
+
 
 print(floodFill(image1, sr1, sc1, color1))
-# print(floodFill(image2, sr2, sc2, color2))
+print(floodFill(image2, sr2, sc2, color2))
+
+## BFS SOLUTION
+# def floodFill(image, sr, sc, color):
+
+#     NROWS = len(image)
+#     NCOLS = len(image[0])
+
+#     if image[sr][sc] == color:
+#         return image
+
+#     queue = deque()
+#     # visited = set()
+
+#     queue.append((sr, sc))
+#     # visited.add((sr, sc))
+
+#     startingColor = image[sr][sc]
+#     image[sr][sc] = color 
+
+#     while queue:
+#         curPos = queue.popleft()
+
+#         directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+#         for direction in directions:
+#             row = curPos[0] + direction[0]
+#             col = curPos[1] + direction[1]
+
+#             if row < 0 or row >= NROWS or col < 0 or col >= NCOLS or image[row][col] != startingColor:
+#                 continue
+                
+#             image[row][col] = color 
+#             queue.append((row, col))
+#             # visited.add((row, col))
+
+#     return image
+    
