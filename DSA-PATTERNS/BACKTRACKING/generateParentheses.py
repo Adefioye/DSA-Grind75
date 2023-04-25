f9278e0de793d1a97b13b3b45093b93a6d0dd580
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-04-25 10:38:26 -0500

    Added second backtracking solution to generate parentheses problem

--- DSA-PATTERNS/BACKTRACKING/generateParentheses.py
+++ DSA-PATTERNS/BACKTRACKING/generateParentheses.py
@@ -5,33 +5,55 @@ n2 = 2
 n1 = 1
 # Output: ["()"]
 
+## SECOND SOLUTION
 def generateParentheses(n):
 
     res = []
-    comb = []
-    OPEN = "("
-    CLOSE = ")"
 
-    def backtrack(openCount, closeCount):
+    def dfs(open, close, path):
 
-        if openCount == closeCount == n:
-            res.append("".join(comb))
+        if open == close == n:
+            res.append(path)
             return 
         
-        if openCount < n:
-            comb.append(OPEN)
-            backtrack(openCount + 1, closeCount)
-            comb.pop()
+        if open == close or open < n:
+            dfs(open + 1, close, path + "(")
+        if open > close and close < n:
+            dfs(open, close + 1, path + ")")
 
-        if closeCount < openCount:
-            comb.append(CLOSE)
-            backtrack(openCount, closeCount + 1)
-            comb.pop()
-
-    backtrack(0, 0)
+    dfs(0, 0, "")
 
     return res 
 
 print(generateParentheses(n1))
 print(generateParentheses(n2))
 print(generateParentheses(n3))
+
+
+## FIRST SOLUTION
+# def generateParentheses(n):
+
+#     res = []
+#     comb = []
+#     OPEN = "("
+#     CLOSE = ")"
+
+#     def backtrack(openCount, closeCount):
+
+#         if openCount == closeCount == n:
+#             res.append("".join(comb))
+#             return 
+        
+#         if openCount < n:
+#             comb.append(OPEN)
+#             backtrack(openCount + 1, closeCount)
+#             comb.pop()
+
+#         if closeCount < openCount:
+#             comb.append(CLOSE)
+#             backtrack(openCount, closeCount + 1)
+#             comb.pop()
+
+#     backtrack(0, 0)
+
+#     return res 
