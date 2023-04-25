e89ff85234bf02d219dd8a8295b1178d6cfc315e
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-04-24 21:53:35 -0500

    Added second backtracking solution to combination sum III problem

--- DSA-PATTERNS/BACKTRACKING/combinationSum3.py
+++ DSA-PATTERNS/BACKTRACKING/combinationSum3.py
@@ -9,24 +9,50 @@ def combinationSum3(k, n):
 
     res = []
     comb = []
+    def dfs(i, total):
 
-    def backtrack(start, total):
-
-        if len(comb) == k and total == n:
+        if total == n and len(comb) == k:
             res.append(comb[:])
             return 
-        
-        if len(comb) > k or total > n or (len(comb) == k and total < n):
-            return 
-        
-        for i in range(start, 10):
-            comb.append(i)
-            backtrack(i + 1, total + i)
-            comb.pop() 
 
-    backtrack(1, 0)
+        if i > 9:
+            return
 
-    return res 
+        # Decision to include i
+        comb.append(i)
+        dfs(i + 1, total + i)
+
+        # Decision not to include i
+        comb.pop()
+        dfs(i + 1, total)
+
+    dfs(1, 0)
+
+    return res
+
+## FIRST SOLUTION
+# def combinationSum3(k, n):
+
+#     res = []
+#     comb = []
+
+#     def backtrack(start, total):
+
+#         if len(comb) == k and total == n:
+#             res.append(comb[:])
+#             return 
+        
+#         if len(comb) > k or total > n or (len(comb) == k and total < n):
+#             return 
+        
+#         for i in range(start, 10):
+#             comb.append(i)
+#             backtrack(i + 1, total + i)
+#             comb.pop() 
+
+#     backtrack(1, 0)
+
+#     return res 
 
 print(combinationSum3(k1, n1))
 print(combinationSum3(k2, n2))
