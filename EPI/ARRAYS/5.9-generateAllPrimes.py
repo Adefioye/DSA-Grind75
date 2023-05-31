03b81fdf47e420d05bf8cf2b98442f52f74170c2
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-05-31 17:17:24 -0500

    Added sieving approach to generate all primes problem

--- EPI/ARRAYS/5.9-generateAllPrimes.py
+++ EPI/ARRAYS/5.9-generateAllPrimes.py
@@ -5,26 +5,43 @@ n2 = 18
 
 # trial-division approach
 # time = ~O(n ^ (3/2)) space = O(n)
+# def generateAllPrimes(n):
+#     res = []
+#     divisorFlag = False
+#     for i in range(2, n + 1):
+
+#         squareRoot = math.floor(i ** (1/2))
+
+#         for j in range(2, squareRoot + 1):
+
+#             if i % j == 0:
+#                 divisorFlag = True
+#                 break
+        
+#         # print(i, divisorFlag)
+#         if not divisorFlag:
+#             res.append(i)
+        
+#         divisorFlag = False
+
+#     return res
+
+# sieving approach (DP)
+# time = O(nloglogn) space = O(n) 
 def generateAllPrimes(n):
+    tab = [True] * (n + 1) 
     res = []
-    divisorFlag = False
+
     for i in range(2, n + 1):
 
-        squareRoot = math.floor(i ** (1/2))
+        if tab[i] == True:
 
-        for j in range(2, squareRoot + 1):
-
-            if i % j == 0:
-                divisorFlag = True
-                break
-        
-        # print(i, divisorFlag)
-        if not divisorFlag:
             res.append(i)
-        
-        divisorFlag = False
+            for i in range(i, n + 1, i):
+                tab[i] = False 
 
     return res
+
 
 print(generateAllPrimes(n1))
 print(generateAllPrimes(n2))
