10fb325b36111e8303dc34590c51c78d7a485ce1
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-06-30 20:42:38 -0500

    Add solution to pow(x, n) problem

--- EPI/PRIMITIVE-TYPES/4.7-computeXpowerY.py
+++ EPI/PRIMITIVE-TYPES/4.7-computeXpowerY.py
@@ -1,20 +1,47 @@
 
 # time = O(log y)
-def power(x, y):
 
-    res, power = 1, y 
 
-    if y < 0:
-        power, x = -power, 1/x 
+def myPow(x: float, n: int) -> float:
 
-    while power:
+    def helper(x, n):
 
-        if power & 1:
-            res *= x
+        if n == 0:
+            return 1
         
-        x, power = x * x, power >> 1 
+        if n % 2 == 0:
+            return helper(x * x, n/2)
+        
+        return x * helper(x, n - 1)
 
-    return res 
+        
+    if n < 0:
+        x, n = 1/ x, -n
+        
+    sign = 1
+    if x < 0 and (n % 2 != 0):
+        sign = -1
+        x = -x
+
+    print(sign)   
+    return sign * helper(x, n)
+        
+
+# def power(x, y):
+
+#     res, power = 1, y 
+
+#     if y < 0:
+#         power, x = -power, 1/x 
+
+#     while power:
+
+#         if power & 1:
+#             res *= x
+        
+#         x, power = x * x, power >> 1 
+
+#     return res 
 
 
 
