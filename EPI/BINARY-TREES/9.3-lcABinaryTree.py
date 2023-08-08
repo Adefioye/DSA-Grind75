d54d7790bccc0ef42679ba86e11a656130e0121b
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-08-07 22:12:38 -0500

    solution to lCA of binary tree

---
+++ EPI/BINARY-TREES/9.3-lcABinaryTree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        if not root:
+            return None
+
+        if root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root 
+
+        if not left and not right:
+            return None 
+        
+        return left if left else right
