b382181577b2a2e80317d453912acde15b051a36
Adefioye <abdulhakeemadefioye@gmail.com>
2024-01-06 11:23:40 -0600

    Changed solution to LCA of BST problem

--- NEETCODE-150/Trees/lcaOfBst.py
+++ NEETCODE-150/Trees/lcaOfBst.py
@@ -14,18 +14,11 @@ class Solution(object):
         :rtype: TreeNode
         """
 
-        if not root:
-            return None
-        if root.val == p.val or root.val == q.val:
-            return root
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-
-        if left and right:
-            return root
-        elif not left and not right:
-            return None
-        elif left:
-            return left
-        else:
-            return right
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
+        
